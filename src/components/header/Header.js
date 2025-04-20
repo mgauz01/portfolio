891dccf0040628099d8a771b1f394a4504b02c58
michael.gringauz <michael.gringauz@gmail.com>
2025-04-20 14:47:53 +0000

    refined header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -30,11 +30,11 @@ const Header = (props) => {
                                 <div id="navbar" className="collapse navbar-collapse navigation-holder">
                                     <button className="menu-close"><i className="ti-close"></i></button>
                                     <ul className="nav navbar-nav mb-2 mb-lg-0">
-                                        <li><Link activeClass="active" to="home" spy={true} smooth={true} duration={500} >Home</Link></li>
+                                        {/* <li><Link activeClass="active" to="home" spy={true} smooth={true} duration={500} >Home</Link></li>
                                         <li>
                                             <Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>About</Link>
-                                        </li>
-                                        <li><Link activeClass="active" to="service" spy={true} smooth={true} duration={500}>Service</Link></li>
+                                        </li> */}
+                                        {/* <li><Link activeClass="active" to="service" spy={true} smooth={true} duration={500}>Service</Link></li> */}
                                         {/* <li>
                                             <Link activeClass="active" to="portfolio" spy={true} smooth={true}  duration={500} onClick={ClickHandler}>Portfolio</Link>
                                         </li> */}
